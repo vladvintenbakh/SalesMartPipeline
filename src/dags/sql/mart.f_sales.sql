f33c5e09a0f48370a6b7e26ff62a551677a56f24
Vlad Vintenbakh <v.vintenbakh@gmail.com>
2025-09-03 12:16:12 +0800

    Added status insertion into f_sales

--- src/dags/sql/mart.f_sales.sql
+++ src/dags/sql/mart.f_sales.sql
@@ -1,6 +1,6 @@
 -- решил пока не добавлять идемпотентность так как это опционально
 
-insert into mart.f_sales (date_id, item_id, customer_id, city_id, quantity, payment_amount)
+insert into mart.f_sales (date_id, item_id, customer_id, city_id, quantity, payment_amount, status)
 select 
     dc.date_id,
     item_id, 
@@ -11,7 +11,8 @@ select
         -- делаю отрицательным только payment_amount, так как по формуле так total_revenue выйдет отрицательным
         when status = 'refunded' then -payment_amount
         else payment_amount
-    end as payment_amount 
+    end as payment_amount,
+    status
 from staging.user_order_log uol
 left join mart.d_calendar as dc on uol.date_time::Date = dc.date_actual
 where uol.date_time::Date = '{{ds}}';
